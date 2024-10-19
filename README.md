8b4a1986ac004777ae69400ab60d2037673e109b
Kadirov Kamoliddin <kadirovkamoliddin24@gmail.com>
2024-10-19 11:25:26 +0400

    Add all files

---
+++ README.md
@@ -0,0 +1,1 @@
+"# Project-My-beginner4" 
---
+++ script.js
@@ -0,0 +1,51 @@
+/*document.body.style.background='red'
+setTimeout(()=>document.body.style.background='green', 3000)
+
+let elem=document.getElementById('elem')
+elem.style.background="black"
+elem.style.color="white"
+elem.style.boxShadow="2px 2px 2px gray"
+
+document.getElementById('p').style.textAlign="center"
+document.getElementById('p').style.fontSize="50px"
+document.getElementById('p').innerHTML="New Text!"
+document.getElementById('myimg').src='mycar.jpg'
+*/
+
+/* function div (){
+    document.getElementById('elem').style.background='red'
+    document.getElementById('elem').style.boxShadow='3px 3px 5px gray'
+    document.getElementsByClassName('div')[0].style.background='green'
+
+
+}
+
+function img(){
+    document.getElementById('myimg').src="car.png"
+}*/
+
+document.getElementById('main').style.background='darksalmon'
+
+document.getElementById('Addtocard').addEventListener('click',functioin())
+{
+    alert('Added to Card')
+}
+
+
+function im(){
+    document.getElementById('imgg').src="car.jpg"
+    document.getElementById('car').style.background='red'
+}
+function imh(){
+    document.getElementById('imgg').src="car2.jpg"
+    document.getElementById('car').style.background='gray'
+}
+function imm(){
+    document.getElementById('imgg').src="car3.jpg"
+    document.getElementById('car').style.background='brown'
+} 
+let div2=document.getElementsByClassName('div2')
+div2.style.background="green"
+div2.style.color="black"
+div2.style.boxShadow="2px 2px 2px black"
+
